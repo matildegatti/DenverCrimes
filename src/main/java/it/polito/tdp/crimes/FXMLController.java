b5a73a2c44dbd59e6575fb21b68a3d00805e4902
Utente <Utente@DESKTOP-S511UBT>
2021-05-17 22:57:17 +0200

    Soluzione videolezione

--- src/main/java/it/polito/tdp/crimes/FXMLController.java
+++ src/main/java/it/polito/tdp/crimes/FXMLController.java
@@ -5,8 +5,12 @@
 package it.polito.tdp.crimes;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 
+import com.sun.tools.javac.util.List;
+
+import it.polito.tdp.crimes.model.Adiacenza;
 import it.polito.tdp.crimes.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -25,16 +29,16 @@ public class FXMLController {
     private URL location;
 
     @FXML // fx:id="boxCategoria"
-    private ComboBox<?> boxCategoria; // Value injected by FXMLLoader
+    private ComboBox<String> boxCategoria; // Value injected by FXMLLoader
 
     @FXML // fx:id="boxMese"
-    private ComboBox<?> boxMese; // Value injected by FXMLLoader
+    private ComboBox<Integer> boxMese; // Value injected by FXMLLoader
 
     @FXML // fx:id="btnAnalisi"
     private Button btnAnalisi; // Value injected by FXMLLoader
 
     @FXML // fx:id="boxArco"
-    private ComboBox<?> boxArco; // Value injected by FXMLLoader
+    private ComboBox<Adiacenza> boxArco; // Value injected by FXMLLoader
 
     @FXML // fx:id="btnPercorso"
     private Button btnPercorso; // Value injected by FXMLLoader
@@ -44,12 +48,40 @@ public class FXMLController {
 
     @FXML
     void doCalcolaPercorso(ActionEvent event) {
-
+    	txtResult.clear();
+    	Adiacenza arco = this.boxArco.getValue();
+    	if(arco == null) {
+    		txtResult.appendText("Seleziona un arco!");
+    		return ;
+    	}
+    	
+    	List<String> percorso = (List<String>) this.model.trovaPercorso(arco.getV1(), arco.getV2());
+    	txtResult.appendText("PERCORSO TRA " + arco.getV1() + " e " + arco.getV2() + ":\n\n");
+    	for(String v : percorso) {
+    		txtResult.appendText(v + "\n");	
+    	}
     }
 
     @FXML
     void doCreaGrafo(ActionEvent event) {
-
+    	txtResult.clear();
+    	String categoria=this.boxCategoria.getValue();
+    	Integer mese=this.boxMese.getValue();
+    	
+    	if(categoria==null || mese==null) {
+    		txtResult.appendText("Seleziona i valori di input");
+    		return;
+    	}
+    	
+    	this.model.creaGrafo(categoria, mese);
+    	LinkedList<Adiacenza> archi = new LinkedList<Adiacenza>(this.model.getArchi());
+    	for(Adiacenza a : archi) {
+    		txtResult.appendText(a.toString());
+    	}
+    	
+    	
+    	//riempio la tendina per il punto 2
+    	this.boxArco.getItems().addAll(archi);
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -65,5 +97,12 @@ public class FXMLController {
     
     public void setModel(Model model) {
     	this.model = model;
+    	this.boxCategoria.getItems().addAll(model.getCategorie());
+    	LinkedList<Integer> mesi=new LinkedList<Integer>();
+    	
+    	for(int i=0;i<=12;i++)
+    		mesi.add(i);
+    	
+    	this.boxMese.getItems().addAll(mesi);
     }
 }
